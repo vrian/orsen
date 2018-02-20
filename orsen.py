0e09ce43f14a70ee80e6cdab4fa6d4cb765bca26
Boss Bryan <jbryanalburo@gmail.com>
2018-02-20 22:57:36 +0800

    next commit

--- orsen.py
+++ orsen.py
@@ -1,9 +1,32 @@
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def hello_world(req, res):
+	print("request: "+req)
+	print("response: "+res)
+	data = {
+		"conversationToken": "",
+		"expectUserResponse": true,
+		"expectedInputs": [{
+			"inputPrompt": {
+				"richInitialPrompt": {
+					"items": [{
+						"simpleResponse": {
+							"textToSpeech": "Howdy! I can tell you fun facts about almost any number, like 42. What do you have in mind?",
+							"displayText": "Howdy! I can tell you fun facts about almost any number. What do you have in mind?"
+						}
+					}],
+					"suggestions": []
+				}
+			},
+			"possibleIntents": [{
+				"intent": "actions.intent.TEXT"
+			}]
+		}]
+	}
+	return jsonify(data)
 
 if __name__ == '__main__':
     app.run()
