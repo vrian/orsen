b8ef060a65a7f80e27e74ee1877702cc3fcd7c04
Boss Bryan <jbryanalburo@gmail.com>
2018-03-17 00:41:12 +0800

    Every tear

--- orsen.py
+++ orsen.py
@@ -13,11 +13,16 @@ def home():
 def orsen():
 
 	#jsonData = request.data
-	requestData = request.get_json()#json.loads(jsonData)
+	#requestData = request.get_json()#json.loads(jsonData)
 	
 	#rawTextQuery = requestData["inputs"]["rawInputs"]["query"]
 
-	data = {"conversationToken":"{\"state\":null,\"data\":{}}","expectUserResponse":True,"expectedInputs":[{"inputPrompt":{"initialPrompts":[{"textToSpeech":"Hello! I am Orsen! What is your name?"+requestData+""}],"noInputPrompts":[]},"possibleIntents":[{"intent":"actions.intent.TEXT"}]}]}
+	if request.method == 'GET':
+		data = {"conversationToken":"{\"state\":null,\"data\":{}}","expectUserResponse":True,"expectedInputs":[{"inputPrompt":{"initialPrompts":[{"textToSpeech":"Hello! I am Orsen! What is your name?"+"GET"+""}],"noInputPrompts":[]},"possibleIntents":[{"intent":"actions.intent.TEXT"}]}]}
+	
+	elif request.method == 'POST':
+		data = {"conversationToken":"{\"state\":null,\"data\":{}}","expectUserResponse":True,"expectedInputs":[{"inputPrompt":{"initialPrompts":[{"textToSpeech":"Hello! I am Orsen! What is your name?"+"POST"+""}],"noInputPrompts":[]},"possibleIntents":[{"intent":"actions.intent.TEXT"}]}]}
+	
 	
 	return jsonify(data)
 
