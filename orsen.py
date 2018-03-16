458e51f646c73a338b91834cc46b61d41a048500
Boss Bryan <jbryanalburo@gmail.com>
2018-03-17 00:55:09 +0800

    sleepless night

--- orsen.py
+++ orsen.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def home():
 	return jsonify({"Page":"Home"})
 	
-@app.route('/orsen/', methods=["POST"])
+@app.route('/orsen/', methods=["GET","POST"])
 def orsen():
 
 	#jsonData = request.data
