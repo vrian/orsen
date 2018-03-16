0baa7485dbe371c284086ea007ef3f5124f51e01
Boss Bryan <jbryanalburo@gmail.com>
2018-03-16 19:51:44 +0800

    forgot to commit lol

--- orsen.py
+++ orsen.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello_world():
 
 	data = {
-		"conversationToken": "",
+		"conversationToken": "{'state':null,'data':{}}",
 		"expectUserResponse": True,
 		"expectedInputs": [{
 			"inputPrompt": {
@@ -26,6 +26,36 @@ def hello_world():
 		}]
 	}
 	return jsonify(data)
+	
+	
+@app.route('/orsen/', methods=["GET","POST"])
+def orsen():
+
+    error = ''
+    try:
+	
+        if request.method == "POST":
+		
+            print(request.form['expectedInputs'])
+            #attempted_password = request.form['password']
+
+            #flash(attempted_username)
+            #flash(attempted_password)
+
+            #if attempted_username == "admin" and attempted_password == "password":
+            #    return redirect(url_for('dashboard'))
+				
+            #else:
+            #    error = "Invalid credentials. Try Again."
+
+        #return render_template("login.html", error = error)
+
+    except Exception as e:
+        #flash(e)
+        #return render_template("login.html", error = error)
+
+	return jsonify(data)
+		
 
 if __name__ == '__main__':
     app.run()
